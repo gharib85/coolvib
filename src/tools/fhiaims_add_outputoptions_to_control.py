3b50b2d7e40afca676e402fdc00c2c8b368ec26e
Reinhard Maurer <reinhard.maurer@yale.edu>
2015-04-08 14:25:39 -0400

    added script to enable correct H,S, wvfn, eigenvalue output in FHI-AIMS

---
+++ src/tools/fhiaims_add_outputoptions_to_control.py
@@ -0,0 +1,61 @@
+#!/bin/python
+"""
+This script takes an FHI-AIMS output generated with 
+keyword 'output k_point_list' and prints 
+"""
+
+from sys import argv
+import numpy as np
+
+if len(argv)<2:
+    print 'Execute this script the following way:'
+    print 'fhiaims_add_outputoptions_to_control.py <OUTPUT_filename>'
+    print 'OUTPUT_filename has to be an FHI-AIMS OUTPUT file generated with keyword '
+    print "'output k_point_list '"
+else:
+    print 'Reading FHI-AIMS Output file'
+
+    filename = argv[1]
+    lines = open(filename,'r').readlines()
+    kpoints = []
+    for l,line in enumerate(lines):
+        #if 'k_point_list' in line:
+            #pass
+        #else:
+            #raise RuntimeError('OUTPUT file has to have been generated with \
+                    #keyword: output k_point_list.')
+
+        if 'K-points in task  ' in line:
+            n_line = l+1
+            nkpts = int(line.split()[-1])
+            for nk in range(nkpts):
+                kpoint = []
+                kpoint.append(float(lines[n_line].split()[4]))
+                kpoint.append(float(lines[n_line].split()[5]))
+                kpoint.append(float(lines[n_line].split()[6]))
+                kpoint.append(float(lines[n_line].split()[9]))
+                kpoints.append(kpoint)
+                n_line += 1
+
+    kpoints = np.array(kpoints)
+
+    print kpoints
+    print ''
+    n_kpts = len(kpoints)
+    n_bands = int(n_kpts/2)
+    
+    output = '#add this to your control.in file'
+    output += 'output k_point_list \n'
+    
+    for n in range(n_bands):
+        output += 'output band  '
+        for i in range(2):
+            for xyz in range(3):
+                output += ' {0:8.4f} '.format(kpoints[n*2+i,xyz])
+        output += ' 2 \n'
+
+    output += 'output eigenvectors \n' 
+    output += 'output overlap_matrix \n' 
+    output += 'output hamiltonian_matrix \n' 
+
+    print output
