0bcd6e8aefa9e135ca0d2d5d03b99103bf3e74ea
Reinhard Maurer <reinhard.maurer@yale.edu>
2017-01-30 14:51:19 -0500

    updated example 1

--- examples/example1/__init__.py
+++ examples/example1/__init__.py
@@ -48,7 +48,9 @@ in positive and negative directions for atom3 and the cartesian x component (c0 
 Calculating the spectral function
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-Using :py:func:`coolvib.convenience.workflow_tensor.workflow_tensor.calculate_spectral_function` we can calculate the spectral functions along all cartesian components and all combinations of cartesian components. This constitutes the main work, including loops over all possible excitations and construction of the electron-phonon spectral function on a discrete grid. This discretization is determined by following keywords:
+This is an optional step when calculating the friction tensor and at the moment only serves analysis purposes.
+
+Using :py:func:`coolvib.convenience.workflow_tensor.workflow_tensor.calculate_spectral_function` we can calculate the spectral functions along all cartesian components and all combinations of cartesian components. This includes loops over all possible excitations and construction of the electron-phonon spectral function on a discrete grid. This discretization is determined by following keywords:
 
     *discretization_type*
         Specifies which broadening function is used to discretize the individual excitations, for more details see :py:func:`coolvib.routines.discretize_peak`
@@ -71,7 +73,7 @@ The calculated spectral functions can be printed to file using :py:func:`coolvib
 Calculating the friction tensor and analysis
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-In order to finally arrive at the friction tensor one has to integrate the spectral functions 
+In order to finally arrive at the friction tensor one either has to integrate the spectral functions 
 over a delta function (see :py:func:`coolvib.routines.evaluate_delta_function`). This is where the 
 main approximations of the underlying method are applied. The width, position and shape of the 
 delta function are controlled by following keywords:
@@ -87,6 +89,45 @@ delta function are controlled by following keywords:
 
 Convergence with respect to the decay rates and lifetimes has to be validated for the above parameters.
 
-The function :py:func:`coolvib.convenience.workflow_tensor.workflow_tensor.calculate_friction_tensor` performs this integration and sets up the tensor. Analysis and printing of the tensor, its eigenvalues and eigenfunctions as well as the inverse of the eigenvalues (the principal lifetimes) can be done using :py:func:`coolvib.convenience.workflow_tensor.workflow_tensor.analyse_friction_tensor`.
+The function :py:func:`coolvib.convenience.workflow_tensor.workflow_tensor.calculate_friction_tensor_from_spectrum` performs this integration and sets up the tensor. 
+
+One can directly calculate the friction tensor by using :py:func:`coolvib.convenience.workflow_tensor.calculate_friction_tensor`, as it is done in this example
+
+Analysis and printing of the tensor, its eigenvalues and eigenfunctions as well as the inverse of the eigenvalues (the principal lifetimes) can be done using :py:func:`coolvib.convenience.workflow_tensor.workflow_tensor.analyse_friction_tensor`.
+
+Results
+^^^^^^^
+
+The expected results are 
+
+Friction Tensor
+      
+             0                     1                     2                     3                     4                     5  
+     0 3.1366e-02 7.7414e-07 7.7374e-06 -1.6675e-02 -2.7957e-07 -3.0235e-06 
+     1 7.7414e-07 3.1365e-02 7.0913e-06 1.3343e-08 -1.6675e-02 -2.6273e-06 
+     2 7.7374e-06 7.0913e-06 9.0953e-03 -2.1645e-06 -2.1907e-06 -2.8059e-03 
+     3 -1.6675e-02 1.3343e-08 -2.1645e-06 9.6877e-03 -1.2865e-07 6.4511e-07 
+     4 -2.7957e-07 -1.6675e-02 -2.1907e-06 -1.2865e-07 9.6872e-03 4.9371e-07 
+     5 -3.0235e-06 -2.6273e-06 -2.8059e-03 6.4511e-07 4.9371e-07 1.6467e-03 
+      
+ 
+Friction Eigenvalues in 1/ps
+         0          1          2          3          4          5
+    0.0006     0.0006     0.0007     0.0100     0.0404     0.0404
+Friction Eigenvectors
+      
+             0                     1                     2                     3                     4                     5  
+     0 -1.3381e-01 4.5781e-01 -1.7908e-03 1.4193e-04 4.9189e-01 -7.2838e-01 
+     1 -4.5780e-01 -1.3383e-01 -2.1490e-03 1.3969e-04 -7.2838e-01 -4.9190e-01 
+     2 -1.4942e-03 6.0177e-04 3.1725e-01 -9.4834e-01 -5.3986e-05 -3.4853e-04 
+     3 -2.4659e-01 8.4361e-01 -3.3172e-03 -3.1598e-04 -2.6695e-01 3.9527e-01 
+     4 -8.4361e-01 -2.4659e-01 -3.9656e-03 -2.7458e-04 3.9528e-01 2.6694e-01 
+     5 -5.1806e-03 2.2797e-03 9.4832e-01 3.1726e-01 1.5499e-05 1.2534e-04 
+      
+Principal lifetimes in ps
+         0          1          2          3          4          5
+ 1566.7157  1566.3840  1412.3889    99.6617    24.7440    24.7431
+
+
 
 """
--- examples/example1/example1.py
+++ examples/example1/example1.py
@@ -30,43 +30,52 @@ active_atoms = [3,4] # meaning we have two atoms - C - 0 and O - 1
 
 model = coolvib.workflow_tensor(system, code='aims', active_atoms=active_atoms)
 
-finite_difference_incr = 0.001
+finite_difference_incr = 0.0025
 
 keywords = {
     'discretization_type' : 'gaussian',
     'discretization_broadening' : 0.01,
-    'discretization_length' : 0.001,
+    'discretization_length' : 0.01,
     'max_energy' : 3.00,
     'temperature' : 300,
     'delta_function_type': 'gaussian',
     'delta_function_width': 0.60,
     'perturbing_energy' : 0.0,
+    'debug': 0,
         }
 
 print 'workflow initialized and keywords set'
 
 ######READ QM INPUT DATA###
 model.read_input_data(
-        spin=True, 
+        spin=False, 
         path='./CO-on-Cu100',
-        filename='OUTPUT', 
+        filename='aims.out', 
         active_atoms=active_atoms, 
         incr=finite_difference_incr,
+        debug=0,
         )
 print 'successfully read QM input data'
 
+############
+#We can either calculate the spectral function and evaluate friction 
+#from there (discretization commands)
+
 ######CALCULATE SPECTRAL FUNCTION###
-model.calculate_spectral_function(mode='default', **keywords)
-#model.read_spectral_function()
-print 'successfully calculated spectral_function'
-model.print_spectral_function('nacs-spectrum.out')
+#model.calculate_spectral_function(mode='default', **keywords)
+# model.read_spectral_function()
+# print 'successfully calculated spectral_function'
+# model.print_spectral_function('nacs-spectrum.out')
+# model.calculate_friction_tensor_from_spectrum(**keywords)
+# model.plot_spectral_function()
 
 #######CALCULATE FRICTION TENSOR###
-model.calculate_friction_tensor(**keywords)
+
+#or we directly calculate friction
+model.calculate_friction_tensor( **keywords)
+
 print 'successfully calculated friction tensor'
 
 model.analyse_friction_tensor()
-
 model.print_jmol_friction_eigenvectors()
 
-#model.plot_spectral_function()
